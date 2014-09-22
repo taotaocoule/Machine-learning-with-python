2df11b8838d2ce6a6ce57566a622db8f1ed49958
taotaocoule <2443877012@qq.com>
2014-09-22 11:32:21 +0800

    K neighbour
    In this file there is the code of K neighbour.

---
+++ K/K neighbour.py
@@ -0,0 +1,15 @@
+from numpy import *
+import operator
+def classify(inX,dataset,labels,k):
+    datasetsize=dataset.shape[0]
+    diffmat=tile(inX,(datasetsize,1))-dataset
+    sqdiffmat=diffmat**2
+    sqdistance=sqdiffmat.sum(axis=1)
+    distance=sqdistance**0.5
+    sortdistance=distance.argsort()
+    classcount={}
+    for i in range(k):
+        votelabel=labels(sortdistance[i])
+        classcount[votelabel]=classcount.get(votelabel,0)+1
+        sortclasscount=sorted(classcount.iteritems(),key=operator.itemgetter(1),reverse=True)
+        return sortclasscount[0][0]
